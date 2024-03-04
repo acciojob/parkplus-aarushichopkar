e5052c60ddb73d24eaa19a6093b3509e21ca9d68
aarushichopkar <aarushi.chopkar@gmail.com>
2024-03-04 22:05:02 +0530

    pay

--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -2,11 +2,14 @@ package com.driver.services.impl;
 
 import com.driver.model.Payment;
 import com.driver.model.PaymentMode;
+import com.driver.model.Reservation;
 import com.driver.repository.PaymentRepository;
 import com.driver.repository.ReservationRepository;
 import com.driver.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
@@ -18,6 +21,31 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
 
-        return null;
+        Optional<Reservation> optionalReservation = reservationRepository2.findById(reservationId);
+        Reservation reservation = optionalReservation.get();
+
+        int bill = reservation.getSpot().getPricePerHour()*reservation.getNumberOfHours();
+        if(amountSent<bill){
+            throw new Exception("Insufficient Amount");
+        }else{
+            mode = mode.toUpperCase();
+            if(mode.equals(PaymentMode.CARD.toString()) || mode.equals(PaymentMode.CASH.toString()) || mode.equals(PaymentMode.UPI.toString())){
+                Payment payment = new Payment();
+                payment.setPaymentCompleted(true);
+                if ("CASH".equals(mode)) {
+                    payment.setPaymentMode(PaymentMode.CASH);
+                } else if ("CARD".equals(mode)) {
+                    payment.setPaymentMode(PaymentMode.CARD);
+                } else{
+                    payment.setPaymentMode(PaymentMode.UPI);
+                }
+                payment.setReservation(reservation);
+                reservationRepository2.save(reservation);
+                return paymentRepository2.save(payment);
+            }
+            else {
+                throw new Exception("Payment mode not detected");
+            }
+        }
     }
 }
